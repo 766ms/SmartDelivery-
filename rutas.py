98489ae8b8b03c1ce98f3d75e3b9206ad0ec334c
Sara <soflovesarianaet@outlook.com>
2026-02-26 10:56:02 -0500

    Se agregó kwargs

--- rutas.py
+++ rutas.py
@@ -1,11 +1,16 @@
-def rutas (barrio, km, precios) :
-    return barrio, km, precios
-barrio=  ["marbella", "ciudad jardin", "bocagrande", "manga"]
-km= [2,14,2,3]
-precios= [2000, 5000, 1000, 2000]
-print("0. marbella - 2 km - $2000")
-print("1. ciudad jardin - 14 km - $5000")
-print("2. bocagrande - 2 km - $1000")
-print("3. manga - 3 km - $2000") 
+
+def rutas(*args):
+    return args
+
+barrio, km, precios = rutas(
+    ["marbella", "ciudad jardin", "bocagrande", "manga"],
+    [2, 14, 2, 3],
+    [2000, 5000, 1000, 2000]
+)
+
+for i in range(len(barrio)):
+    print(f"{i}. {barrio[i]} - {km[i]} km - ${precios[i]}")
+
 selectbarrio = int(input("Escoge el número del barrio: "))
+
 print(f"El barrio seleccionado es {barrio[selectbarrio]} y se encuentra a {km[selectbarrio]} km de nuestra sede del centro y su envio tiene un valor de ${precios[selectbarrio]}")
